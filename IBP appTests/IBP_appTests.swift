2f9b18dd82491d4f7fb08796b028d2f58806916c
Florian Lepont <florian.l@enchanted.tools>
2026-03-04 10:27:13 +0100

    Initial Commit

---
+++ IBP appTests/IBP_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  IBP_appTests.swift
+//  IBP appTests
+//
+//  Created by Florian Lepont on 04/03/2026.
+//
+
+import Testing
+
+struct IBP_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
